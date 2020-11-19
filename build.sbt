ae9a5e778367f296e711202168923f78a8e0f896
adrianhitz <32393038+adrianhitz@users.noreply.github.com>
2020-11-19 23:42:48 +0100

    Project setup

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "advent-of-code-2020"
+
+version := "1.0"
+
+scalaVersion := "2.13.3"
---
+++ src/main/scala/astrionic/adventofcode2020/solutions/day01/Day01.scala
@@ -0,0 +1,17 @@
+package astrionic.adventofcode2020.solutions.day01
+
+import astrionic.adventofcode2020.framework.AdventSolution
+
+object Day01 extends AdventSolution {
+
+  writeSolution = true
+  executePart = ExecutePart.One
+
+  override def solvePart1(input: String): String = {
+    input
+  }
+
+  override def solvePart2(input: String): String = {
+    ???
+  }
+}
