24e1c3036764edc6f4f659c0c080c3eea6d5d32c
adrianhitz <32393038+adrianhitz@users.noreply.github.com>
2020-12-01 07:53:30 +0100

    Add solution for day 1 part 2

--- src/main/scala/astrionic/adventofcode2020/solutions/day01/Day01.scala
+++ src/main/scala/astrionic/adventofcode2020/solutions/day01/Day01.scala
@@ -2,35 +2,47 @@ package astrionic.adventofcode2020.solutions.day01
 
 import astrionic.adventofcode2020.framework.AdventSolution
 
-import scala.collection.immutable.HashSet
-import scala.collection.mutable
-
 object Day01 extends AdventSolution {
-
-  // writeSolution = true
-  executePart = ExecutePart.One
 
   override def solvePart1(input: String): String = {
     val nums = input.split("\n").toList.map(_.toInt)
     val target = 2020
 
     twoSum(nums, target) match {
-      case Some((num0, num1)) => (num0 * num1).toString
-      case None               => "No solution found"
+      case Some((n0, n1)) => (n0 * n1).toString
+      case None           => "No solution found"
     }
   }
 
   override def solvePart2(input: String): String = {
-    ???
+    val nums = input.split("\n").toList.map(_.toInt)
+    val target = 2020
+
+    threeSum(nums, target) match {
+      case Some((n0, n1, n2)) => (n0 * n1 * n2).toString
+      case None               => "No solution found"
+    }
   }
 
   def twoSum(nums: List[Int], target: Int): Option[(Int, Int)] = {
     val set = nums.toSet
-    nums.filter(n => set.contains(target - n)) match {
-      case x :: _ => Some(x, target - x)
-      case Nil    => None
+    for(n <- nums) {
+      val complement = target - n
+      if(set.contains(complement)) {
+        return Some(n, complement)
+      }
     }
+    None
   }
 
-
+  def threeSum(nums: List[Int], target: Int): Option[(Int, Int, Int)] = {
+    for(n0 <- nums) {
+      val complement = target - n0
+      twoSum(nums, complement) match {
+        case Some((n1, n2)) => return Some(n0, n1, n2)
+        case None           =>
+      }
+    }
+    None
+  }
 }
