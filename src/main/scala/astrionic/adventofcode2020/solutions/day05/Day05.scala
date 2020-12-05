99aea605a7ba4bf98e6f0278adbd1daa86bc9db9
adrianhitz <32393038+adrianhitz@users.noreply.github.com>
2020-12-05 11:35:53 +0100

    Simplify day 5 solution

--- src/main/scala/astrionic/adventofcode2020/solutions/day05/Day05.scala
+++ src/main/scala/astrionic/adventofcode2020/solutions/day05/Day05.scala
@@ -4,14 +4,10 @@ import astrionic.adventofcode2020.framework.AdventSolution
 
 object Day05 extends AdventSolution {
 
-  override def solvePart1(input: String): String = {
-    val passes = parseInput(input)
-    passes.map(_.getId).max.toString
-  }
+  override def solvePart1(input: String): String = parseIds(input).max.toString
 
   override def solvePart2(input: String): String = {
-    val passes = parseInput(input)
-    val idsSorted = passes.map(_.getId).sorted
+    val idsSorted = parseIds(input).sorted
     val offset = idsSorted.head
     val firstMismatch = idsSorted.zipWithIndex
       .filter(id => id._1 != id._2 + offset)
@@ -21,27 +17,10 @@ object Day05 extends AdventSolution {
     myPass.toString
   }
 
-  private def parseInput(input: String): Array[BoardingPass] = {
+  private def parseIds(input: String): Array[Int] =
     input
       .replaceAll("[FL]", "0")
       .replaceAll("[BR]", "1")
       .split('\n')
-      .map(b => BoardingPass.fromBinaryString(b))
-  }
-
-  private case class BoardingPass(row: Int, col: Int) {
-    def getId: Int = row * 8 + col
-  }
-
-  private object BoardingPass {
-    def fromBinaryString(b: String): BoardingPass = {
-      val rowBinary = b.slice(0, 7)
-      val colBinary = b.slice(7, 10)
-
-      val rowDecimal = Integer.parseInt(rowBinary, 2)
-      val colDecimal = Integer.parseInt(colBinary, 2)
-
-      BoardingPass(rowDecimal, colDecimal)
-    }
-  }
+      .map(b => Integer.parseInt(b, 2))
 }
