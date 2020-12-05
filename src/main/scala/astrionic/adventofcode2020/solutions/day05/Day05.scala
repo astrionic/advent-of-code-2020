95eb7ba9e5b365079519b1a9592dc727bcbed407
adrianhitz <32393038+adrianhitz@users.noreply.github.com>
2020-12-05 11:19:38 +0100

    Add solution for day 5 part 2

--- src/main/scala/astrionic/adventofcode2020/solutions/day05/Day05.scala
+++ src/main/scala/astrionic/adventofcode2020/solutions/day05/Day05.scala
@@ -4,30 +4,44 @@ import astrionic.adventofcode2020.framework.AdventSolution
 
 object Day05 extends AdventSolution {
 
-  writeSolution = true
-  executePart = ExecutePart.Two
-
   override def solvePart1(input: String): String = {
-    val binary = input
-      .replaceAll("[FL]", "0")
-      .replaceAll("[BR]", "1")
-
-    val ids = binary
-      .split('\n')
-      .map(b => {
-        val rowBinary = b.slice(0, 7)
-        val colBinary = b.slice(7, 10)
-
-        val rowDecimal = Integer.parseInt(rowBinary, 2)
-        val colDecimal = Integer.parseInt(colBinary, 2)
-
-        rowDecimal * 8 + colDecimal
-      })
-
-    ids.max.toString
+    val passes = parseInput(input)
+    passes.map(_.getId).max.toString
   }
 
   override def solvePart2(input: String): String = {
-    ???
+    val passes = parseInput(input)
+    val idsSorted = passes.map(_.getId).sorted
+    val offset = idsSorted.head
+    val firstMismatch = idsSorted.zipWithIndex
+      .filter(id => id._1 != id._2 + offset)
+      .head
+      ._1
+    val myPass = firstMismatch - 1
+    myPass.toString
+  }
+
+  private def parseInput(input: String): Array[BoardingPass] = {
+    input
+      .replaceAll("[FL]", "0")
+      .replaceAll("[BR]", "1")
+      .split('\n')
+      .map(b => BoardingPass.fromBinaryString(b))
+  }
+
+  private case class BoardingPass(row: Int, col: Int) {
+    def getId: Int = row * 8 + col
+  }
+
+  private object BoardingPass {
+    def fromBinaryString(b: String): BoardingPass = {
+      val rowBinary = b.slice(0, 7)
+      val colBinary = b.slice(7, 10)
+
+      val rowDecimal = Integer.parseInt(rowBinary, 2)
+      val colDecimal = Integer.parseInt(colBinary, 2)
+
+      BoardingPass(rowDecimal, colDecimal)
+    }
   }
 }
