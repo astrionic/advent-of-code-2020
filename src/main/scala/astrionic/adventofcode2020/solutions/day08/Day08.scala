645187f182f1211c689b5c0dae581147b0aca151
adrianhitz <32393038+adrianhitz@users.noreply.github.com>
2020-12-08 18:59:18 +0100

    Add solution for day 8 part 2

--- src/main/scala/astrionic/adventofcode2020/solutions/day08/Day08.scala
+++ src/main/scala/astrionic/adventofcode2020/solutions/day08/Day08.scala
@@ -2,13 +2,11 @@ package astrionic.adventofcode2020.solutions.day08
 
 import astrionic.adventofcode2020.framework.AdventSolution
 
+//noinspection DuplicatedCode
 object Day08 extends AdventSolution {
 
-  // writeSolution = true
-  executePart = ExecutePart.One
-
   override def solvePart1(input: String): String = {
-    val program: Array[Instruction] = parseInput(input)
+    val program: List[Instruction] = parseInput(input)
 
     var acc = 0
     var pc = 0
@@ -33,12 +31,45 @@ object Day08 extends AdventSolution {
   }
 
   override def solvePart2(input: String): String = {
-    ???
+    val program: List[Instruction] = parseInput(input)
+    val programs: List[List[Instruction]] = createProgramPermutations(program)
+
+    for(p <- programs) {
+      var terminated = false
+      var error = false
+      var acc = 0
+      var pc = 0
+      var previouslyExecuted = Set[Int]()
+
+      while(!terminated && !error && !previouslyExecuted.contains(pc)) {
+        if(pc == p.length) {
+          terminated = true
+        } else if(pc > p.length || pc < 0) {
+          error = true
+        } else {
+          previouslyExecuted += pc
+
+          p(pc) match {
+            case Instruction("acc", value) =>
+              acc += value
+              pc += 1
+            case Instruction("jmp", value) =>
+              pc += value
+            case Instruction("nop", _) =>
+              pc += 1
+            case Instruction(_, _) => throw new Exception("Invalid operation")
+          }
+        }
+      }
+
+      if(terminated) return acc.toString
+    }
+    return "No solution found"
   }
 
   private case class Instruction(name: String, value: Int)
 
-  private def parseInput(input: String): Array[Instruction] = {
+  private def parseInput(input: String): List[Instruction] = {
     input
       .split('\n')
       .map(line => {
@@ -46,6 +77,19 @@ object Day08 extends AdventSolution {
         val name = parts(0)
         val value = parts(1).toInt
         Instruction(name, value)
+      })
+      .toList
+  }
+
+  private def createProgramPermutations(program: List[Instruction]) = {
+    program.zipWithIndex
+      .filter(i => i._1.name == "jmp" || i._1.name == "nop")
+      .map(i => {
+        val newInstr = i._1 match {
+          case Instruction("jmp", value) => Instruction("nop", value)
+          case Instruction("nop", value) => Instruction("jmp", value)
+        }
+        program.updated(i._2, newInstr)
       })
   }
 }
