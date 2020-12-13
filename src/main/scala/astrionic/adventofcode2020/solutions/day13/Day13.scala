a59206697778b0641fd4563dd1c2d1ddaed2d4fc
adrianhitz <32393038+adrianhitz@users.noreply.github.com>
2020-12-13 14:00:19 +0100

    Add solution for day 13 part 2

--- src/main/scala/astrionic/adventofcode2020/solutions/day13/Day13.scala
+++ src/main/scala/astrionic/adventofcode2020/solutions/day13/Day13.scala
@@ -3,15 +3,9 @@ package astrionic.adventofcode2020.solutions.day13
 import astrionic.adventofcode2020.framework.AdventSolution
 
 object Day13 extends AdventSolution {
-
-  writeSolution = true
-  executePart = ExecutePart.One
-
   override def solvePart1(input: String): String = {
     val (earliestDeparture, idsWithXs) = parseInput(input)
-    val ids = idsWithXs
-      .filter(id => id.matches("^\\d+\\z"))
-      .map(_.toInt)
+    val ids: Array[Int] = idsWithXs.flatten // Get rid of 'x' entries
 
     val earliestBuses = ids.map(id => (id, ((earliestDeparture / id) + 1) * id))
     val earliestBus = earliestBuses.minBy(_._2)
@@ -21,13 +15,31 @@ object Day13 extends AdventSolution {
   }
 
   override def solvePart2(input: String): String = {
-    ???
+    val (_, idPositions: Array[Option[Int]]) = parseInput(input)
+    val ids: Array[(Int, Int)] = idPositions.zipWithIndex.flatMap(extractOption)
+
+    var start = 0L
+    var step = 1L
+
+    for((id, i) <- ids) {
+      while((start + i) % id != 0) {
+        start += step
+      }
+      step *= id
+    }
+    
+    start.toString
   }
 
-  private def parseInput(input: String): (Int, Array[String]) = {
+  private def parseInput(input: String): (Int, Array[Option[Int]]) = {
     val lines = input.split('\n')
     val earliestDeparture = lines(0).toInt
-    val busIds = lines(1).split(',')
+    val busIds = lines(1).split(',').map(_.toIntOption)
     (earliestDeparture, busIds)
+  }
+
+  private def extractOption[A, B](x: (Option[A], B)): Option[(A, B)] = x match {
+    case (Some(a), b) => Some((a, b))
+    case (None, _)    => None
   }
 }
