0da6f8deeb8d86dfb2d1d1e6a4e9624ddd95f8c2
adrianhitz <32393038+adrianhitz@users.noreply.github.com>
2020-12-15 18:22:10 +0100

    Improve day 15 performance

--- src/main/scala/astrionic/adventofcode2020/solutions/day15/Day15.scala
+++ src/main/scala/astrionic/adventofcode2020/solutions/day15/Day15.scala
@@ -2,11 +2,13 @@ package astrionic.adventofcode2020.solutions.day15
 
 import astrionic.adventofcode2020.framework.AdventSolution
 
+import scala.collection.mutable
+
 object Day15 extends AdventSolution {
 
   override def solvePart1(input: String): String = solve(input, 2020)
 
-  // Takes about 500 times longer than part 1, around 25 seconds on my machine
+  // Takes about 250 times as long as part 1 (around 9 seconds on my current machine)
   override def solvePart2(input: String): String = solve(input, 30000000)
 
   def parseInput(input: String): List[Int] =
@@ -15,17 +17,20 @@ object Day15 extends AdventSolution {
   def solve(input: String, turns: Int): String = {
     val startingNumbers = parseInput(input)
 
-    var map: Map[Int, Int] = startingNumbers
-      .slice(0, startingNumbers.length - 1)
-      .zip(1 until startingNumbers.length)
-      .toMap
+    val map = mutable.Map[Int, Int]()
+    map.addAll(
+      startingNumbers
+        .slice(0, startingNumbers.length - 1)
+        .zip(1 until startingNumbers.length)
+        .toMap
+    )
     var previousNum = startingNumbers.last
     for(turn <- startingNumbers.length + 1 to turns) {
       val currentNum = map.get(previousNum) match {
         case Some(n) => (turn - 1) - n
         case None    => 0
       }
-      map += (previousNum -> (turn - 1))
+      map.addOne(previousNum -> (turn - 1))
       previousNum = currentNum
     }
     previousNum.toString
