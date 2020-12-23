70842acdc3e2877fc5908af66d4025a83d22419f
adrianhitz <32393038+adrianhitz@users.noreply.github.com>
2020-12-23 22:21:47 +0100

    Add solution for day 22 part 2

--- src/main/scala/astrionic/adventofcode2020/solutions/day22/Day22.scala
+++ src/main/scala/astrionic/adventofcode2020/solutions/day22/Day22.scala
@@ -1,35 +1,34 @@
 package astrionic.adventofcode2020.solutions.day22
 
 import astrionic.adventofcode2020.framework.AdventSolution
+import astrionic.adventofcode2020.solutions.day22.Day22.Player.Player
 
 object Day22 extends AdventSolution {
 
-  // writeSolution = true
-  executePart = ExecutePart.One
+  writeSolution = true
+  executePart = ExecutePart.Two
 
   override def solvePart1(input: String): String = {
-    var (p1, p2) = parseInput(input)
+    var (deck1, deck2) = parseInput(input)
 
-    while(p1.nonEmpty && p2.nonEmpty) {
-      if(p1.head > p2.head) {
-        p1 = p1.tail ++ List(p1.head, p2.head)
-        p2 = p2.tail
+    while(deck1.nonEmpty && deck2.nonEmpty) {
+      if(deck1.head > deck2.head) {
+        deck1 = deck1.tail ++ List(deck1.head, deck2.head)
+        deck2 = deck2.tail
       } else {
-        p2 = p2.tail ++ List(p2.head, p1.head)
-        p1 = p1.tail
+        deck2 = deck2.tail ++ List(deck2.head, deck1.head)
+        deck1 = deck1.tail
       }
     }
 
-    val winningDeck = if(p1.nonEmpty) p1 else p2
-    winningDeck
-      .zip((1 to winningDeck.length).reverse)
-      .map(x => x._1 * x._2)
-      .sum
-      .toString
+    val winningDeck = if(deck1.nonEmpty) deck1 else deck2
+    calculateWinningScore(winningDeck).toString
   }
 
   override def solvePart2(input: String): String = {
-    ???
+    val (deck1, deck2) = parseInput(input)
+    val winningDeck = recursiveCombat(deck1, deck2).winningDeck
+    calculateWinningScore(winningDeck).toString
   }
 
   private def parseInput(input: String): (List[Int], List[Int]) = {
@@ -37,6 +36,61 @@ object Day22 extends AdventSolution {
     input match {
       case regex(p1, p2) => (p1.split('\n').map(_.toInt).toList, p2.split('\n').map(_.toInt).toList)
       case _             => throw new Exception
+    }
+  }
+
+  private def calculateWinningScore(winningDeck: List[Int]): Int = {
+    winningDeck
+      .zip((1 to winningDeck.length).reverse)
+      .map(x => x._1 * x._2)
+      .sum
+  }
+
+  private[day22] object Player extends Enumeration {
+    type Player = Value
+    val One, Two = Value
+  }
+
+  private case class GameResult(winner: Player, winningDeck: List[Int])
+
+  private def recursiveCombat(deck1: List[Int], deck2: List[Int]): GameResult = {
+    var previousStates: Set[(List[Int], List[Int])] = Set()
+    var (d1, d2): (List[Int], List[Int]) = (deck1, deck2)
+
+    while(d1.nonEmpty && d2.nonEmpty) {
+      // If this state has happened before player 1 wins immediately
+      if(previousStates.contains((d1, d2))) {
+        return GameResult(Player.One, d1)
+      }
+
+      // Save state
+      val state = (d1, d2)
+      previousStates += state
+
+      // Draw cards
+      val (c1, c2) = (d1.head, d2.head)
+      d1 = d1.tail
+      d2 = d2.tail
+
+      // Determine winner
+      val roundWinner = if(d1.length >= c1 && d2.length >= c2) {
+        // Shahrazad
+        recursiveCombat(d1.slice(0, c1), d2.slice(0, c2)).winner
+      } else {
+        if(c1 > c2) Player.One else Player.Two
+      }
+
+      // Put cards on the bottom of the winner's deck
+      roundWinner match {
+        case Player.One => d1 :++= List(c1, c2)
+        case Player.Two => d2 :++= List(c2, c1)
+      }
+    }
+
+    if(d1.nonEmpty) {
+      GameResult(Player.One, d1)
+    } else {
+      GameResult(Player.Two, d2)
     }
   }
 }
